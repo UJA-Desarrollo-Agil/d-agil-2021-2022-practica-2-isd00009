a5f0281a473e81ee11c588e9dad36569083a0b60
isd00009 <isd00009@red.ujaen.es>
2022-03-15 10:31:19 +0100

    Juego final

--- games/media/games/tutorial/Juego.js
+++ games/media/games/tutorial/Juego.js
@@ -7,7 +7,7 @@
 /* A unique id for your game. This is never displayed. I use a UUID,
  * but you can use anything that is guaranteed unique (a URL you own,
  * or a variation on your email address, for example). */
-undum.game.id = "89353480-ec80-11e1-aff1-0800200c9a66"; // GEnerado por http://www.famkruithof.net/uuid/uuidgen
+undum.game.id = "df5d5600-a32b-11ec-b230-0800200c9a66"; // GEnerado por http://www.famkruithof.net/uuid/uuidgen
 
 /* A string indicating what version of the game this is. Versions are
  * used to control saved-games. If you change the content of a game,
@@ -22,16 +22,39 @@ jQuery.fx.off = false
 undum.game.situations = {
     comienzo: new undum.SimpleSituation(
         "<h1>Tu cuarto</h1>\
-        	<p>Suena el despertador a las 7:30, después de haber dormido apenas 5 horas, ya que estuviste toda la noche dandole vueltas al partido de hoy. Tienes un día bastante largo por delante, por la mañana debes ir a clase\
+        <img src='./media/img/habitacion.jpg' class='float_right' width='150' height='200'>\
+          <p>Suena el despertador a las 7:30, después de haber dormido apenas 5 horas, ya que estuviste toda la noche dandole vueltas al partido de hoy. Tienes un día bastante largo por delante, por la mañana debes ir a clase\
 				y posteriormente tienes que ir a Madrid para ver el partido en el Bernabéu.\
-				Tras perder 5 minutos en Twitter, tienes que decidir entre <a href='ducha'>ducharte</a> o <a href='desayuno'>desayunar</a> para no llegar tarde a la clase de Desarrollo Ágil</p>"
+				Tras perder 5 minutos en Twitter, tienes que decidir entre <a href='./nocomer'>ducharte</a> o <a href='./comer'>desayunar</a> para no llegar tarde a la clase de Desarrollo Ágil</p>", {
+            actions: {
+                'nocomer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre + 20);
+                    system.doLink('ducha');
+                },
+                'comer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre - 20);
+                    system.doLink('desayuno');
+                }
+            }
+        }
     ),
     ducha: new undum.SimpleSituation(
         "<h1>En la universidad</h1>\
 			<p>Has llegado a clase justo a las 8:30, pero tienes que esperar a que llegue Victor, que viene un poco tarde.\
 				En clase continuas haciendo tu práctica de undum, que se entrega la semana que viene y aun ni has empezado (como de costumbre). A mitad de clase empiezas a notar \
-				algo de hambre y te arrepientes de no haber desayunado antes. Al terminar la clase, piensas en <a href='cafeteria'>ir a la cafetería</a> para desayunar, ya que te espera un largo viaje hacia Madrid. Pero por otro lado\
-				piensas en <a href='irmadrid'>ir ya hacia Madrid</a> para así llegar antes, comer allí y descansar el resto del día hasta el partido.</p>"
+				algo de hambre y te arrepientes de no haber desayunado antes. Al terminar la clase, piensas en <a href='./comer'>ir a la cafetería</a> para desayunar, ya que te espera un largo viaje hacia Madrid. Pero por otro lado\
+				piensas en <a href='./nocomer'>ir ya hacia Madrid</a> para así llegar antes, comer allí y descansar el resto del día hasta el partido.</p>", {
+            actions: {
+                'nocomer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre + 20);
+                    system.doLink('irmadrid');
+                },
+                'comer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre - 20);
+                    system.doLink('cafeteria');
+                }
+            }
+        }
 
     ),
     desayuno: new undum.SimpleSituation(
@@ -50,17 +73,50 @@ undum.game.situations = {
     irmadrid: new undum.SimpleSituation(
         "<h1>En Madrid</h1>\
             <p>Ya has llegado a Madrid, tu hotel esta situado a apenas 500 metros del estadio y hay un gran ambiente en los alrededores. Te tumbas en la cama del hotel y te empiezas a quedar dormido, pero recuerda que es la hora de comer\
-            y posiblemente no vuelvas a comer hasta despues del partido. Así que si no tienes mucha hambre puedes salir a <a href='vuelta'>dar una vuelta</a> para ver el ambiente o <a href='comer'>quedarte en el hotel para comer</a> y descansar un rato.</p>"
+            y posiblemente no vuelvas a comer hasta despues del partido. Así que si no tienes mucha hambre puedes salir a <a href='./nocomer'>dar una vuelta</a> para ver el ambiente o <a href='./sicomer'>quedarte en el hotel para comer</a> y descansar un rato.</p>", {
+            actions: {
+                'nocomer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre + 20);
+                    if (character.qualities.hambre >= 100) {
+                        system.doLink('inanicion');
+                    } else {
+                        system.doLink('vuelta');
+                    }
+                },
+                'sicomer': function(character, system, action) {
+                    system.setQuality("hambre", character.qualities.hambre - 20);
+                    system.doLink('comer');
+                }
+            }
+        }
+    ),
+    inanicion: new undum.SimpleSituation(
+        "<h1>FIN DEL JUEGO</h1>\
+            <p>Has muerto de hambre y has sido llevado a tu habitación en ataúd. Estar tanto tiempo sin comer ha sido una mala idea.Ya nunca verás a Mbappé\
+            vestido de blanco ni la gran remontada de aquella noche.</p>\
+            <h1>FIN</h1>"
     ),
     vuelta: new undum.SimpleSituation(
         "<h1>En la calle</h1>\
             <p>Sales a la calle y vas camino al Bernabéu, te encuentras con una agrupación de gente que tiene pinta de ser violenta y estar drogada, con antorchas, banderas del Real Madrid y de España recitando cánticos del equipo y haciendo el saludo romano.\
-            Son integrantes de Ultras Sur. Al ver a esta gente comienzas a temblar y piensas en <a href='volverhotel'>volver al hotel</a>, pero otra parte de ti quiere <a href='seguirestadio'>seguir hacia el estadio.</a></p>"
+            Son integrantes de Ultras Sur. Al ver a esta gente comienzas a temblar y piensas en <a href='volverhotel'>volver al hotel</a>, pero otra parte de ti quiere <a href='seguirestadio'>seguir hacia el estadio.</a></p>\
+            <img src='./media/img/ultras.jpg' width='512' height='380'>"
     ),
     comer: new undum.SimpleSituation(
         "<h1>En el estadio</h1>\
-            <p>Ya has comido y descansado, te encuentras en el estadio y faltan 5 minutos para empezar el partido. La megafonía empieza a decir los nombres de los jugadores del Madrid y todo el estadio aplaude para animar. Más tarde nombran a los jugadores\
-            del PSG y cuando nombran a Mbappé decides <a href='aplaudir'>aplaudir</a> o <a href='pitar'>pitar</a>.</p>"
+            <p>Ya has comido y descansado y te encuentras en el estadio, faltan 5 minutos para empezar el partido. La megafonía empieza a decir los nombres de los jugadores del Madrid y todo el estadio aplaude para animar. Más tarde nombran a los jugadores\
+            del PSG y cuando nombran a Mbappé decides <a href='./nopitar'>aplaudir</a> o <a href='./sipitar'>pitar</a>.</p>", {
+            actions: {
+                'sipitar': function(character, system, action) {
+                    system.setQuality("posibilidadFichaje", character.qualities.posibilidadFichaje - 15);
+                    system.doLink('pitar');
+                },
+                'nopitar': function(character, system, action) {
+                    system.setQuality("posibilidadFichaje", character.qualities.posibilidadFichaje + 40);
+                    system.doLink('aplaudir');
+                }
+            }
+        }
     ),
     seguirestadio: new undum.SimpleSituation(
         "<h1>FIN DEL JUEGO</h1>\
@@ -71,17 +127,60 @@ undum.game.situations = {
     volverhotel: new undum.SimpleSituation(
         "<h1>En el estadio</h1>\
             <p>Volviste al hotel sano y salvo, te encuentras en el estadio y faltan 5 minutos para empezar el partido. La megafonía empieza a decir los nombres de los jugadores del Madrid y todo el estadio aplaude para animar. Más tarde nombran a los jugadores\
-            del PSG y cuando nombran a Mbappé decides <a href='aplaudir'>aplaudir</a> o <a href='pitar'>pitar</a>.</p>"
+            del PSG y cuando nombran a Mbappé decides <a href='./nopitar'>aplaudir</a> o <a href='./sipitar'>pitar</a>.</p>", {
+            actions: {
+                'sipitar': function(character, system, action) {
+                    system.setQuality("posibilidadFichaje", character.qualities.posibilidadFichaje - 15);
+                    system.doLink('pitar');
+                },
+                'nopitar': function(character, system, action) {
+                    system.setQuality("posibilidadFichaje", character.qualities.posibilidadFichaje + 40);
+                    system.doLink('aplaudir');
+                }
+            }
+        }
     ),
     aplaudir: new undum.SimpleSituation(
         "<h1>En el estadio</h1>\
             <p>Mbappé devuelve el aplauso y sonríe. El PSG se adelanta en la primera mitad gracias a un gol de Mbappé. El partido acaba 0-2 en el marcador global al descanso, por lo que el Madrid lo tiene muy difícil para remontar.\
-            Tras el descanso, Benzema aprovecha un error del portero del PSG para marcar el primero. Este gol cambia totalmente la dinámica del partido y el Madrid termina remontando el partido en tan solo 15 minutos.</p1>"
+            Tras el descanso, Benzema aprovecha un error del portero del PSG para marcar el primero. Este gol cambia totalmente la dinámica del partido y el Madrid termina remontando el partido en tan solo 15 minutos. Al terminar el partido\
+            Mbappé toma una decision. <a href='./fichaje'>Ver decisión.</a></p1>", {
+            actions: {
+                'fichaje': function(character, system, action) {
+                    if (Math.floor(Math.random() * 100) < 90)
+                        system.doLink('fichado');
+                    else
+                        system.doLink('nofichado');
+                }
+            }
+        }
     ),
     pitar: new undum.SimpleSituation(
         "<h1>En el estadio</h1>\
             <p>Mbappé muestra un rostro serio y marcha hacia el terreno de juego. El PSG se adelanta en la primera mitad gracias a un gol de Mbappé. El partido acaba 0-2 en el marcador global al descanso, por lo que el Madrid lo tiene muy difícil para remontar.\
-            Tras el descanso, Benzema aprovecha un error del portero del PSG para marcar el primero. Este gol cambia totalmente la dinámica del partido y el Madrid termina remontando el partido en tan solo 15 minutos.</p1>"
+            Tras el descanso, Benzema aprovecha un error del portero del PSG para marcar el primero. Este gol cambia totalmente la dinámica del partido y el Madrid termina remontando el partido en tan solo 15 minutos. Al terminar el partido\
+            Mbappé toma una decision. <a href='./fichaje'>Ver decisión.</a></p1>", {
+            actions: {
+                'fichaje': function(character, system, action) {
+                    if (Math.floor(Math.random() * 100) < 35)
+                        system.doLink('fichado');
+                    else
+                        system.doLink('nofichado');
+                }
+            }
+        }
+    ),
+    fichado: new undum.SimpleSituation(
+        "<h1>MBAPPÉ FICHA POR EL REAL MADRID</h1>\
+            <p>El jugador francés acaba fichando por el Real Madrid. El traspaso se hará efectivo este verano y ficha por 6 temporadas.</p>\
+            <img src='./media/img/mbappemadrid.jpg' width='512' height='340'>\
+            <h1>FIN</h1>"
+    ),
+    nofichado: new undum.SimpleSituation(
+        "<h1>MBAPPÉ SE QUEDA EN EL PSG</h1>\
+            <p>El club parisino ha ofrecido al jugador 50 millones anuales y rechaza la oferta del Real Madrid.</p>\
+            <img src='./media/img/mbappecarcel.jpg' width='512' height='340'>\
+            <h1>FIN</h1>"
     )
 };
 
